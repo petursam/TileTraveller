9c656010e5ec66f23a0fe6bc55a9bd653169b882
Petur Saemundsen <petursam@gmail.com>
2020-09-17 09:59:48 +0000

    setti if setningu fyrir tile 1,1

--- Tile_traveller.py
+++ Tile_traveller.py
@@ -1,2 +1,9 @@
-#Tile Traveller commence
-2 + 2 = 4
+def first(n):
+    if n == "N" or "n":
+        input("You can travel: (N)orth, (E)ast or (S)outh")
+    else: print("Not a valid direction!")
+
+
+direction = input("Direction: ")
+
+print(first(direction))
