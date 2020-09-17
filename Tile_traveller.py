ca45d1dce6f0cd93db841103e7fe70cdc042ab93
Petur Saemundsen <petursam@gmail.com>
2020-09-17 12:19:11 +0000

    almost there

--- Tile_traveller.py
+++ Tile_traveller.py
@@ -1,4 +1,4 @@
-print("You can travel (N)orth.")
+
 
 north = "You can travel (N)orth"
 east = "You can travel (E)ast"
@@ -12,18 +12,98 @@ east_south = "You can travel (E)ast or (S)outh"
 east_west = "You can travel (E)ast or (W)est"
 south_west = "You can travel (S)outh or (W)est"
 
-def one_one(d):
+
+def one_one():
+    print(north)
+    d = input("Direction: ")
     if d == "N" or d == "n":
-        return north_east_south
-        
-    else: return "Not a valid direction!"
+        one_two()
+    else: 
+        print("Not a valid direction!")
+        one_one()
 
-def one_two(d):
-    if d == "S":
-        return one_one(direction)
-    else: return "hello world"
+def one_two():
+    print(north_east_south)
+    d = input("Direction: ")
+    if d == "S" or d == "s":
+        one_one()
+    elif d == "E" or d == "e":
+        two_two()
+    elif d == "N" or d == "n":
+        one_three()
+    else:
+        print("Not a valid direction!")
+        one_two()
+
+def one_three():
+    print(east_south)
+    d = input("Direction: ")
+    if d == "E" or d == "e":
+        two_three()
+    elif d == "S" or d == "s":
+        one_two()
+    else:
+        print("Not a valid direction!")
+        one_three()
+
+def two_three():
+    print(east_west)
+    d = input("Direction: ")
+    if d == "E" or d == "e":
+        three_three()
+    elif d == "W" or d == "w":
+        one_three()
+    else:
+        print("Not a valid direction!")
+        two_three()
+
+def two_two():
+    print(south_west)
+    d = input("Direction: ")
+    if d == "S" or d == "s":
+        two_one()
+    elif d == "W" or d == "w":
+        one_two()
+    else:
+        print("Not a valid direction!")
+        two_two()
+
+def two_one():
+    print(north)
+    d = input("Direction: ")
+    if d == "N" or d == "n":
+        two_two()
+    else:
+        print("Not a valid direction!")
+        two_one()
+
+def three_three():
+    print(south_west)
+    d = input("Direction: ")
+    if d == "S" or d == "s":
+        three_two()
+    elif d == "W" or d == "w":
+        two_three()
+    else:
+        print("Not a valid direction")
+        three_three()
+
+def three_two():
+    print(north_south)
+    d = input("Direction: ")
+    if d == "N" or d == "n":
+        three_three()
+    elif d == "S" or d == "s":
+        three_one()
+    else:
+        print("Not a valid direction")
+        three_two()
+
+def three_one():
+    victory = "Victory!"
+    return victory
     
-direction = input("Direction: ")
+    
 
-print(one_one(direction))
-direction = input("Direction: ")
+print(one_one())
+
