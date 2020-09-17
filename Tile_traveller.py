df881b200f0cec309435c2a01ce85b321aae0938
Petur Saemundsen <petursam@gmail.com>
2020-09-17 10:25:38 +0000

    fixed it probably

--- Tile_traveller.py
+++ Tile_traveller.py
@@ -1,9 +1,15 @@
-def first(n):
-    if n == "N" or "n":
-        input("You can travel: (N)orth, (E)ast or (S)outh")
-    else: print("Not a valid direction!")
+print("You can travel (N)orth.")
+def one_one(d):
+    if d == "N":
+        return "You can travel: (N)orth, (E)ast or (S)outh"
+    elif d == "n":
+        return "You can travel: (N)orth, (E)ast or (S)outh"
+        
+    else: return "Not a valid direction!"
+    
 
 
 direction = input("Direction: ")
 
-print(first(direction))
+print(one_one(direction))
+input("Direction: ")
